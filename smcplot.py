97a42334161be1df56839f9bad46081de18cbc07
aylwyn <aylwyn@cantab.net>
2014-10-19 19:42:46 +0100

    switch to argparse; add multple subplots via plotlist arg

--- smcplot.py
+++ smcplot.py
@@ -2,7 +2,7 @@
 # Aylwyn Scally 2014
 
 import sys
-import optparse
+import argparse
 import os
 import os.path
 import glob
@@ -28,222 +28,229 @@ class SMCPlot(object):
 	def show(self):
 		print("\t".join([self.ptype, self.path, self.pname, self.pcol]))
 
-p = optparse.OptionParser()
-p.add_option('--hc', action='store_true', default = False)
-p.add_option('--nodisplay', action='store_true', default = False)
-#p.add_option('--colours', default = '')
-p.add_option('--title', default = '')
-#p.add_option('--recycle_colours', action='store_true', default = False)
-p.add_option('--geneflow', action='store_true', default = False)
-p.add_option('--coalrates', action='store_true', default = False)
-p.add_option('--nescale', action='store_true', default = False)
-p.add_option('--showvals', action='store_true', default = False)
-p.add_option('--noplot', action='store_true', default = False)
-p.add_option('--psmc', action='store_true', default = False)
-p.add_option('--msmc', action='store_true', default = False)
-p.add_option('--run_psmcplot', action='store_true', default = False)
-p.add_option('-m', '--msmcfile', default = [], action='append')
-p.add_option('-M', '--msmcdir', default = [], action='append')
-p.add_option('-p', '--psmcfile', default = [], action='append')
-p.add_option('-P', '--psmcdir', default = [], action='append')
-p.add_option('-u', '--mugen', type='float', default = 1.25e-8)
-p.add_option('-t', '--tgen', type='float', default = 25.0)
-p.add_option('--maxy', type='float', default = 0.0)
-p.add_option('--maxx', type='float', default = 0.0)
-p.add_option('--legend', type='int', default = 0, help='0: no legend; else location specified as in matplotlib Legend()')
-p.add_option('-s', '--simfile', default='')
-p.add_option('-f', '--plotlist', default='', help = 'Each line:  ptype, path, name, colour, linestyle, linewidth')
-p.add_option('--plotlist_bold', default='', help = 'plot file for bold lines (format as PLOTLIST)')
-p.add_option('-o', '--outname', default='')
-p.add_option('-v', '--verbose', action='store_true', default = False)
+p = argparse.ArgumentParser()
+p.add_argument('--hc', action='store_true', default = False)
+p.add_argument('--nodisplay', action='store_true', default = False)
+p.add_argument('--title', default = '')
+#p.add_argument('--geneflow', action='store_true', default = False)
+#p.add_argument('--coalrates', action='store_true', default = False)
+#p.add_argument('--nescale', action='store_true', default = False)
+p.add_argument('--showvals', action='store_true', default = False)
+p.add_argument('--noplot', action='store_true', default = False)
+#p.add_argument('--psmc', action='store_true', default = False)
+#p.add_argument('--msmc', action='store_true', default = False)
+p.add_argument('--run_psmcplot', action='store_true', default = False)
+#p.add_argument('-m', '--msmcfile', default = [], action='append')
+#p.add_argument('-M', '--msmcdir', default = [], action='append')
+#p.add_argument('-p', '--psmcfile', default = [], action='append')
+#p.add_argument('-P', '--psmcdir', default = [], action='append')
+p.add_argument('-u', '--mugen', type=float, default = 1.25e-8)
+p.add_argument('-t', '--tgen', type=float, default = 25.0)
+p.add_argument('--maxy', type=float, default = 0.0)
+p.add_argument('--maxx', type=float, default = 0.0)
+p.add_argument('--legend', type=int, default = 0, help='<0: no legend; else location specified as in matplotlib Legend()')
+p.add_argument('--label', default='')
+p.add_argument('orient', nargs='?', choices=['vertical', 'horizontal'], default='vertical')
+#p.add_argument('-s', '--simfile', default='')
+p.add_argument('-f', '--plotlist', nargs = '*', default='', help = 'Each line:  ptype, path, name, colour, linestyle, linewidth')
+p.add_argument('-o', '--outname', default='smc')
+p.add_argument('-v', '--verbose', action='store_true', default = False)
 
-opt, args = p.parse_args()
+args = p.parse_args()
 
-if opt.nodisplay:
+if args.nodisplay:
 	matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 loglevel = logging.WARNING
-if opt.verbose:
+if args.verbose:
 	loglevel = logging.INFO
 logging.basicConfig(format = '%(module)s:%(lineno)d:%(levelname)s: %(message)s', level = loglevel)
 
-if opt.run_psmcplot or opt.msmcdir or opt.msmcfile:
-	info('Scaling with mu = %e per gen and tgen = %.1f' % (opt.mugen, opt.tgen))
+if args.run_psmcplot:# or args.msmcdir or args.msmcfile:
+	info('Scaling with mu = %e per gen and tgen = %.1f' % (args.mugen, args.tgen))
 
-if opt.hc:
+nplots = len(args.plotlist)
+
+if args.hc:
 	from matplotlib import rcParams
 	rcParams['axes.labelsize'] = 6
 	rcParams['xtick.labelsize'] = 6
 	rcParams['ytick.labelsize'] = 6
 	rcParams['legend.fontsize'] = 6
+	fsize = [2.25, 2.25*nplots]
 
-inputlist = []
-plotfiles = []
+if args.orient == 'vertical':
+	fig, ax = plt.subplots(nplots, 1, squeeze = False)
+elif args.orient ==  'horizontal':
+	fig, ax = plt.subplots(1, nplots, squeeze = False)
+axv = ax.reshape(-1)
 
-if opt.plotlist_bold:
-	lalph = '0.3'
-else:
-	lalph = '1.0'
+sdir = os.path.abspath('.')
+for ip, ppath in enumerate(args.plotlist):
+	os.chdir(sdir)
+	pdir, pfile = os.path.split(ppath)
+	if pdir:
+		info('switching to directory %s' % pdir)
+		os.chdir(pdir)
 
-if opt.plotlist:
-	for tok in (line.split() for line in open(opt.plotlist)):
-		inputlist.append(SMCPlot(*tok + [lalph]))
-
-if opt.plotlist_bold:
-	for tok in (line.split() for line in open(opt.plotlist_bold)):
-		inputlist.append(SMCPlot(*tok))
-
-msmcdirs = opt.msmcdir
-psmcdirs = opt.psmcdir
-if opt.msmc:
-	msmcdirs += args
-if opt.psmc:
-	psmcdirs += args
-for x in msmcdirs:
-	inputlist.append(SMCPlot('msmcdir', x, os.path.basename(x)))
-for x in psmcdirs:
-	inputlist.append(SMCPlot('psmcdir', x, os.path.basename(x)))
-
-for x in opt.msmcfile:
-	plotfiles.append(SMCPlot('msmcfile', x, x))
-for x in opt.psmcfile:
-	plotfiles.append(SMCPlot('psmcfile', x, x))
-for x in opt.simfile:
-	plotfiles.append(SMCPlot('simfile', x, x))
-
-for splot in inputlist:
-	resfiles = []
-	if splot.ptype in ('msmcfile', 'psmcfile', 'simfile'):
-		plotfiles.append(splot)
-		continue
-
-	if splot.ptype == 'msmcdir':
-		resfiles = glob.glob(os.path.join(splot.path, '*.final.txt'))
-
-	if splot.ptype == 'psmcdir':
-		if opt.run_psmcplot:
-			cwd = os.path.abspath('.')
-			os.chdir(splot.path)
-			for psmcfile in glob.glob('*psmc.out'):
-				pref = psmcfile.replace('psmc.out', '') 
-				if not pref:
-					pref = '1'
-				cmd = 'psmc_plot.pl -u %e -g %f -R %s %s' % (opt.mugen, opt.tgen, pref, psmcfile)
-				info('running psmc_plot.pl in %s' % splot.path)
-				aosutils.subcall(cmd, sim=False, wait = True)
-			os.chdir(cwd)
-		resfiles = glob.glob(os.path.join(splot.path, '*.0.txt'))
-
-	if not resfiles:
-		error('no results files found in %s' % splot.path)
-	else:
-		if len(resfiles) > 1:
-			error('multiple results files found in %s; using first' % splot.path)
-		splot.ptype = splot.ptype.replace('dir', 'file')
-		splot.path = resfiles[0]
-		plotfiles.append(splot)
-
-fig = plt.figure()
-if opt.hc:
-	fsize = [2.25, 2.25]
-	mar = [0.15, 0.15, 0.037, 0.036]
-	margins = [mar[0], mar[1], 1-mar[2]-mar[0], 1-mar[3]-mar[1]]
-	ax = plt.Axes(fig, margins)
-	fig.add_axes(ax)
-
-maxy = 0
-maxx = 0
-for ir, splot in enumerate(plotfiles):
-	info(splot.pname)
-
-	if splot.ptype == 'msmcfile':
-		tb=pd.read_table(splot.path, header = 0)
-		rx = opt.tgen*(tb.ix[:,1])/opt.mugen
-		rx[0] = max(rx[0], 1) # to account for log x axis
-		li = len(rx) - 1
-		if opt.coalrates:
-			for ncol, lab in [(3, 'l00'), (4, 'l01'), (5, 'l11')]:#, (3, 'l00')] 
-				if opt.nescale:
-					ry = (1/tb.ix[:,ncol])/(2*opt.mugen)
-				else:
-					ry = tb.ix[:,ncol]
-				plt.step(rx, ry, label = lab, color=coalrates_col[lab])
-	#			plt.text(rx[li], ry[li], sname, fontsize=6)
-				maxy = max(maxy, max(ry))
+	inputlist = []
+	plotfiles = []
+	for tok in (line.split() for line in open(pfile)):
+		if tok[0].startswith('maxx'):
+			args.maxx = float(tok[1])
+		elif tok[0].startswith('maxy'):
+			args.maxy = float(tok[1])
+		elif tok[0].startswith('legend'):
+			args.legend = int(tok[1])
+		elif tok[0].startswith('label'):
+			args.label = tok[1]
 		else:
-			if opt.geneflow:
-				ry = 2 * tb.ix[:,4] / (tb.ix[:,3] + tb.ix[:,5])
+			inputlist.append(SMCPlot(*tok))
+
+	#msmcdirs = args.msmcdir
+	#psmcdirs = args.psmcdir
+	#if args.msmc:
+	#	msmcdirs += args
+	#if args.psmc:
+	#	psmcdirs += args
+	#for x in msmcdirs:
+	#	inputlist.append(SMCPlot('msmcdir', x, os.path.basename(x)))
+	#for x in psmcdirs:
+	#	inputlist.append(SMCPlot('psmcdir', x, os.path.basename(x)))
+	#
+	#for x in args.msmcfile:
+	#	plotfiles.append(SMCPlot('msmcfile', x, x))
+	#for x in args.psmcfile:
+	#	plotfiles.append(SMCPlot('psmcfile', x, x))
+	#for x in args.simfile:
+	#	plotfiles.append(SMCPlot('simfile', x, x))
+
+	for splot in inputlist:
+		resfiles = []
+		if splot.ptype in ('msmcfile', 'psmcfile', 'simfile'):
+			plotfiles.append(splot)
+			continue
+
+		if splot.ptype == 'msmcdir':
+			resfiles = glob.glob(os.path.join(splot.path, '*.final.txt'))
+
+		if splot.ptype == 'psmcdir':
+			if args.run_psmcplot:
+				cwd = os.path.abspath('.')
+				os.chdir(splot.path)
+				for psmcfile in glob.glob('*psmc.out'):
+					pref = psmcfile.replace('psmc.out', '') 
+					if not pref:
+						pref = '1'
+					cmd = 'psmc_plot.pl -u %e -g %f -R %s %s' % (args.mugen, args.tgen, pref, psmcfile)
+					info('running psmc_plot.pl in %s' % splot.path)
+					aosutils.subcall(cmd, sim=False, wait = True)
+				os.chdir(cwd)
+			resfiles = glob.glob(os.path.join(splot.path, '*.0.txt'))
+
+		if not resfiles:
+			error('no results files found in %s' % splot.path)
+		else:
+			if len(resfiles) > 1:
+				error('multiple results files found in %s; using first' % splot.path)
+			splot.ptype = splot.ptype.replace('dir', 'file')
+			splot.path = resfiles[0]
+			plotfiles.append(splot)
+
+	plt.sca(axv[ip])
+
+	maxy = 0
+	maxx = 0
+	for ir, splot in enumerate(plotfiles):
+		debug(splot.pname)
+
+		if splot.ptype == 'msmcfile':
+			tb=pd.read_table(splot.path, header = 0)
+			rx = args.tgen*(tb.ix[:,1])/args.mugen
+			rx[0] = max(rx[0], 1) # to account for log x axis
+			li = len(rx) - 1
+			if args.coalrates:
+				for ncol, lab in [(3, 'l00'), (4, 'l01'), (5, 'l11')]:#, (3, 'l00')] 
+					if args.nescale:
+						ry = (1/tb.ix[:,ncol])/(2*args.mugen)
+					else:
+						ry = tb.ix[:,ncol]
+					plt.step(rx, ry, label = lab, color=coalrates_col[lab])
+		#			plt.text(rx[li], ry[li], sname, fontsize=6)
+					maxy = max(maxy, max(ry))
 			else:
-				ry = (1/tb.ix[:,3])/(2*opt.mugen)
+				if args.geneflow:
+					ry = 2 * tb.ix[:,4] / (tb.ix[:,3] + tb.ix[:,5])
+				else:
+					ry = (1/tb.ix[:,3])/(2*args.mugen)
+				plt.step(rx, ry, label = splot.pname, color=splot.pcol, ls=splot.pls, lw=splot.plw, where='post', alpha=splot.palph)
+		#	li = len(rx) - 1
+		#	plt.text(args.tgen*(tb.ix[li,1])/args.mugen, (1/tb.ix[li,3])/(2*args.mugen), sname, color=icol, fontsize=6)
+		#	plt.text(rx[li], ry[li], sname, color=icol, fontsize=6)
+
+		if splot.ptype == 'psmcfile':
+			tb=pd.read_table(splot.path, header = None)
+
+			rx = tb.ix[:,0]
+			ry = 1e4*tb.ix[:,1]/1e3
+			if args.showvals:
+				for ix, iy in zip(rx, ry):
+					print('%f\t%f' % (ix, iy))
 			plt.step(rx, ry, label = splot.pname, color=splot.pcol, ls=splot.pls, lw=splot.plw, where='post', alpha=splot.palph)
-	#	li = len(rx) - 1
-	#	plt.text(opt.tgen*(tb.ix[li,1])/opt.mugen, (1/tb.ix[li,3])/(2*opt.mugen), sname, color=icol, fontsize=6)
-	#	plt.text(rx[li], ry[li], sname, color=icol, fontsize=6)
 
-	if splot.ptype == 'psmcfile':
-		tb=pd.read_table(splot.path, header = None)
+		if splot.ptype == 'simfile':
+			tb=pd.read_table(splot.path, header = None)
 
-		rx = tb.ix[:,0]
-		ry = 1e4*tb.ix[:,1]/1e3
-		if opt.showvals:
-			for ix, iy in zip(rx, ry):
-				print('%f\t%f' % (ix, iy))
-		plt.step(rx, ry, label = splot.pname, color=splot.pcol, ls=splot.pls, lw=splot.plw, where='post', alpha=splot.palph)
+			rx = tb.ix[:,0]
+			ry = tb.ix[:,1]
+			plt.step(rx, ry, label = splot.pname, color=splot.pcol, ls='--', where='post', alpha=splot.palph)
 
-	if splot.ptype == 'simfile':
-		tb=pd.read_table(splot.path, header = None)
+		maxx = max(maxx, max(rx))
+		maxy = max(maxy, max(ry))
 
-		rx = tb.ix[:,0]
-		ry = tb.ix[:,1]
-		plt.step(rx, ry, label = splot.pname, color=splot.pcol, ls='--', where='post', alpha=splot.palph)
-
-	maxx = max(maxx, max(rx))
-	maxy = max(maxy, max(ry))
-
-plt.xscale('log')
-plt.xlim(1e3, maxx**1.1)
-plt.xlabel('Time (y)')
-if opt.geneflow:
-	plt.ylim(0, 1.1)
-	plt.ylabel(r'Relative coalescent rate')
-else:
-	if opt.maxy <= 0.0:
+	plt.xscale('log')
+	plt.xlim(1e3, maxx**1.1)
+	plt.xlabel('Time (y)')
+#	if args.geneflow:
+#		plt.ylim(0, 1.1)
+#		plt.ylabel(r'Relative coalescent rate')
+#	else:
+	if args.maxy <= 0.0:
 		plt.ylim(0, 1.1*maxy)
 	else:
-		plt.ylim(0, 1.1*opt.maxy)
-	if opt.maxx <= 0.0:
+		plt.ylim(0, 1.1*args.maxy)
+	if args.maxx <= 0.0:
 		plt.xlim(0, 1.1*maxx)
 	else:
-		plt.xlim(0, 1.1*opt.maxx)
-	if not opt.coalrates:
-		plt.ylabel(r'$N_e/1000$')
+		plt.xlim(0, 1.1*args.maxx)
+#	if not args.coalrates:
+	plt.ylabel(r'$N_e/1000$')
 
-if opt.title:
-	plt.title(opt.title)#, fontsize = 'small')
+	if args.title:
+		plt.title(args.title)#, fontsize = 'small')
 
-if opt.coalrates:
-	plt.legend(('l00', 'l01', 'l11'), loc = 2, prop={'size':8})#, ncol = 2), fontsize = 'xx-small')
-elif opt.legend:
-	# remove duplicate labels
-	handles, labels = plt.gca().get_legend_handles_labels()
-	by_label = dict(zip(labels, handles))
-	lg = plt.legend(by_label.values(), by_label.keys(), loc=opt.legend)
-	lg.get_frame().set_linewidth(0.5) 
-#	plt.legend(loc = opt.legend)#, prop={'size':8})#, ncol = 2), fontsize = 'xx-small')
+#	if args.coalrates:
+#		plt.legend(('l00', 'l01', 'l11'), loc = 2, prop={'size':8})#, ncol = 2), fontsize = 'xx-small')
+	if args.legend >= 0:
+		# remove duplicate labels
+		handles, labels = plt.gca().get_legend_handles_labels()
+		by_label = dict(zip(labels, handles))
+		lg = plt.legend(by_label.values(), by_label.keys(), loc=args.legend)
+		lg.get_frame().set_linewidth(0.5) 
+	#	plt.legend(loc = args.legend)#, prop={'size':8})#, ncol = 2), fontsize = 'xx-small')
 
-#plt.tight_layout()
+	if args.label:
+		plt.text(0.03, 0.97, args.label, transform=axv[ip].transAxes, fontsize=9, fontweight='bold', va='top')
 
-if not opt.noplot:
-	if opt.hc:
+os.chdir(sdir)
+
+plt.subplots_adjust(left=0.35/fsize[0],bottom=0.375/fsize[1], right=1 - 0.075/fsize[0], top= 1 - 0.075/fsize[1], hspace = 0.25)
+
+if not args.noplot:
+	if args.hc:
 		fig.set_size_inches(fsize[0], fsize[1])
-		if opt.outname:
-			fname = opt.outname + '.pdf'
-		else:
-			if opt.coalrates:
-				fname = 'msmc-coalrates' + '.pdf'
-			else:
-				fname = 'smc' + '.pdf'
+		fname = args.outname + '.pdf'
 
 		sys.stderr.write('hardcopy in %s\n' % fname)
 		plt.savefig(fname)
